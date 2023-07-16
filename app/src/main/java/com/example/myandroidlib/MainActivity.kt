bc8d116d5784b6e9755f7f00880f559282a7b818
Blair Fernandes <31095601+blair49@users.noreply.github.com>
2023-07-16 15:07:15 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myandroidlib/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.myandroidlib
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import com.blair.texttransformer.TextTransformer
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val btn = findViewById<Button>(R.id.transformButton)
+        val textView = findViewById<EditText>(R.id.myEditText)
+        btn.setOnClickListener {
+            TextTransformer.toggleCase(textView)
+        }
+    }
+}
