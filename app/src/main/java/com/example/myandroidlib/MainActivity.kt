d95b743d3f3a8a3648490fecd51928239d7cbb2d
Blair Fernandes <31095601+blair49@users.noreply.github.com>
2023-07-23 14:54:41 +0530

    Use text-transformer in app module

--- app/src/main/java/com/example/myandroidlib/MainActivity.kt
+++ app/src/main/java/com/example/myandroidlib/MainActivity.kt
@@ -1,10 +1,9 @@
 package com.example.myandroidlib
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.blair.texttransformer.TextTransformer
 
 class MainActivity : AppCompatActivity() {
